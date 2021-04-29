7bfff5546bc04cc5fc8448bb853881b3de9c9238
Christian Zintel <c.zintel@yahoo.de>
2021-04-29 09:02:48 +0200

    finished ex1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const form = document.querySelector('[data-form]')
-const header = document.querySelector('[data-header]')
-const input = document.querySelector('[name="header-text"]')
+const header = document.querySelector([data-header])
+const form = document.querySelector([data-form])
+const input = document.querySelector([name="header-text"])
 
 form.addEventListener('submit', event => {
   event.preventDefault()
